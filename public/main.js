72dddc08ee99a7037dfafb987a33ed2e54cfd109
Michael Lyons <michael.lyons1112@gmail.com>
2023-11-23 17:43:04 +0000

    Adding barebones ui and ally positions

--- public/main.js
+++ public/main.js
@@ -1,7 +1,33 @@
 const tiles = document.querySelectorAll(".tile");
-tiles.forEach((tile) => {
-  tile.addEventListener("click", (tile) => {
-    console.log(tile);
-    tile.target.style.backgroundColor = "red";
-  });
+
+// Ally starting positions
+const allyStartingPositions = [[10,2], [10,3], [11,2], [11,3], [11,4]];
+const allyPositions = [[10,2], [10,3], [11,2], [11,3], [11,4]];
+const allyPositionsDivs = [];
+allyPositions.forEach((position) => {
+  allyPositionsDivs.push(document.querySelectorAll(".tile")[gridConversion(position[0], position[1])]);
 });
+
+allyStartingPositions.forEach((position) => {
+  tiles[gridConversion(position[0], position[1])].style.backgroundColor = "lightgreen";
+});
+
+// Movement
+tiles.forEach((tile) => {
+  if (allyPositionsDivs.includes(tile)) {
+    tile.addEventListener("click", (tile) => {
+      tile.target.style.backgroundColor = "green";
+      tiles.forEach((tile) => {
+        tile.removeEventListener("click")
+      })
+    });
+  }
+});
+
+const options = document.querySelectorAll(".option");
+options[0].addEventListener("click", (option) => {
+  option.target.style.backgroundColor = "green";
+});
+
+// Converts grid coordinates to the matching div
+function gridConversion (x, y) {return (x * 12) + y;};
